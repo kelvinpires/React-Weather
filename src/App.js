9c49c115af8783a925442ba470785999fe3a3db4
Kelvin Pires <pireskelvin7@gmail.com>
2021-09-30 15:40:26 -0300

    :sparkles: Add Form and Weather components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Form } from "./components/Form";
+import { Weather } from "./components/Weather";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState([]);
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  const handleSubmit = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    setWeather((prev) => [...prev, data]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form handleSubmit={handleSubmit} city={(e) => setCity(e.target.value)} />
+
+      {weather.map((item) => (
+        <div key={item.id}>
+          <Weather
+            city={item.name}
+            country={item.sys.country}
+            temp={item.main.temp}
+            maxTemp={item.main["temp_max"]}
+            minTemp={item.main["temp_min"]}
+            icon={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${item.weather[0]["icon"]}.svg`}
+            description={item.weather[0].description}
+          />
+        </div>
+      ))}
     </div>
   );
 }
